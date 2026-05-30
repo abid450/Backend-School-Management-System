3b0842f7f471e6b0a1ffe3b5dcc912c1f9c1956e
Abid Hasan <144882936+abid450@users.noreply.github.com>
2026-05-30 13:48:35 +0600

    Backend School Management

--- JWT/settings.py
+++ JWT/settings.py
@@ -210,6 +210,6 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "onlinetech25764@gmail.com"
-EMAIL_HOST_PASSWORD = "dvpf gzvj miga fpen"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "onlinetech25764@gmail.com"
